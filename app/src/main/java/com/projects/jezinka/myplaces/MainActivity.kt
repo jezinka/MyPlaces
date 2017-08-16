80b2adee1ba473b6b38b56400950050ae6ea6816
jezinka <jezina1989@gmail.com>
2017-08-16 19:07:50 +0200

    create main activity

--- app/src/main/java/com/projects/jezinka/myplaces/MainActivity.kt
+++ app/src/main/java/com/projects/jezinka/myplaces/MainActivity.kt
@@ -2,11 +2,15 @@ package com.projects.jezinka.myplaces
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        longtitude.setText("54.32124323")
+        latitude.setText("56.432321232")
     }
 }
