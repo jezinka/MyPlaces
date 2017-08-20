f0fe1fcc80d0e77a022b9cc1b5888b4d413d5375
jezinka <jezina1989@gmail.com>
2017-08-20 09:01:29 +0200

    add database for saving places

---
+++ app/src/main/java/com/projects/jezinka/myplaces/MyPlacesContract.kt
@@ -0,0 +1,18 @@
+package com.projects.jezinka.myplaces
+
+import android.provider.BaseColumns
+
+
+object MyPlacesContract {
+
+    class MyPlaceEntry : BaseColumns {
+        companion object {
+            val TABLE_NAME = "my_place"
+
+            val _ID = "_id"
+            val COLUMN_NAME_LONGTITUDE = "longtitude"
+            val COLUMN_NAME_LATITUDE = "latitude"
+            val COLUMN_NAME_NOTE = "note"
+        }
+    }
+}
---
+++ app/src/main/java/com/projects/jezinka/myplaces/MyPlacesDBHelper.kt
@@ -0,0 +1,43 @@
+package com.projects.jezinka.myplaces
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class MyPlacesDBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    private val SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + MyPlacesContract.MyPlaceEntry.Companion.TABLE_NAME + " (" +
+                    MyPlacesContract.MyPlaceEntry.Companion._ID + " INTEGER PRIMARY KEY," +
+                    MyPlacesContract.MyPlaceEntry.Companion.COLUMN_NAME_NOTE + " TEXT," +
+                    MyPlacesContract.MyPlaceEntry.Companion.COLUMN_NAME_LONGTITUDE + " NUMBER," +
+                    MyPlacesContract.MyPlaceEntry.Companion.COLUMN_NAME_LATITUDE + " NUMBER)"
+
+    private val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + MyPlacesContract.MyPlaceEntry.Companion.TABLE_NAME
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(SQL_CREATE_ENTRIES)
+    }
+
+    override fun onOpen(db: SQLiteDatabase) {
+        super.onOpen(db)
+        db.execSQL("PRAGMA foreign_keys=ON")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES)
+        onCreate(db)
+    }
+
+    override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        onUpgrade(db, oldVersion, newVersion)
+    }
+
+    companion object {
+        // If you change the database schema, you must increment the database version.
+        val DATABASE_VERSION = 1
+        val DATABASE_NAME = "myPlaces.db"
+    }
+}
